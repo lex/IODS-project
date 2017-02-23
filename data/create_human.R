ce3ac3a5b6cbd09fb57edf5f3468c324e8945bc0
lex <lex@oispa.beer>
2017-02-23 22:12:53 +0200

    add week 5 data wrangling

--- data/create_human.R
+++ data/create_human.R
@@ -1,4 +1,6 @@
-library(dplyr)
+# Atte Keltanen
+# Thu Feb 23 21:55:06 EET 2017
+# Data wrangling of HDI data
 
 # Get the datasets
 # More information available here:
@@ -6,6 +8,10 @@ library(dplyr)
 # http://hdr.undp.org/sites/default/files/hdr2015_technical_notes.pdf
 hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
 gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
+
+library(dplyr)
+
+setwd(".")
 
 summary(hd)
 dim(hd)
@@ -46,3 +52,35 @@ colnames(hdi_gii)
 
 # Save it for further use
 write.csv(hdi_gii, file="human.csv", row.names=FALSE)
+
+# More wrangling from now on for the new week
+
+# 1. Transform gni to be numeric instead of strings
+hdi_gii$gni <- as.numeric(gsub(",", "", hdi_gii$gni))
+
+# 2. Drop unwanted columns
+columns_to_keep <- c("country", 
+                     "secondaryEducationFemale", 
+                     "secondaryEducationMale", 
+                     "expectedYearsOfEducation", 
+                     "lifeExpectancy", 
+                     "gni", 
+                     "maternalMortalityRatio", 
+                     "adolescentBirthRate", 
+                     "representationInParliament"
+                     )
+
+hdi_gii <- select(hdi_gii, one_of(columns_to_keep))
+
+# 3. Remove rows with missing values
+hdi_gii <- hdi_gii[complete.cases(hdi_gii),]
+
+# 4. Remove regions
+hdi_gii <- head(hdi_gii, -7)
+
+# 5. Change row names to countries and drop the country column
+row.names(hdi_gii) <- hdi_gii$country
+hdi_gii[1] = NULL
+
+# Save the data again for further use
+write.csv(hdi_gii, file="human.csv")
